d27ff115a172933e313c7e0e97532588775e4dee
Therese Nagengast <nagengtm@gmail.com>
2022-04-11 18:11:59 -0400

    attempting to start one in python

--- 2021/1/one.js
+++ 2021/1/one.js
@@ -1,6 +1,6 @@
 /* 
   Day 1: use the day one input file and find every day that the depth increased
-  Notes: started with reading the file as this:  const readFile = fs.readFileSync('day_one_input.txt', 'utf8').split('\n')
+  Notes: started with reading the file as this:  const readFile = fs.readFileSync('input.txt', 'utf8').split('\n')
     This got me to 1138
     Need to look up the .filter and .map to understand why it gives the extra 1 to get the correct answer.
 */
