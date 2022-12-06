c96f0cbec01b04ad8b1af7f74cbca0c2a1966f5f
Therese Nagengast <nagengtm@gmail.com>
2022-12-05 20:04:57 -0500

    set up for day 5

---
+++ 2022/5/stacks.js
@@ -0,0 +1,16 @@
+/*
+  
+*/
+
+//import libs
+const { readFileSync } = require('fs');
+
+const stacks = async () => {
+  // read the file, split on double new line
+  const strat = readFileSync('input.txt', 'utf8').trim().split('\n')
+  
+
+
+}
+
+stacks();
