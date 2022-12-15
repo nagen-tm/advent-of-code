6fe14820428aa180585f99ef2dc4863c1509c925
Therese Nagengast <nagengtm@gmail.com>
2022-12-15 18:51:47 -0500

    day nine set up

---
+++ 2022/9/planks.js
@@ -0,0 +1,13 @@
+/*
+*/
+
+//import libs
+const { readFileSync } = require('fs');
+const lines = readFileSync('example.txt', 'utf8').trim().split('\n')
+
+// parse the file system information for movement, dir, file and size
+function planks(lines) {
+
+  return ;
+}
+
