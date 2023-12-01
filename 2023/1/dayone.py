9e48066447349262a7f02c6dac8638e122b8015c
Therese Nagengast <nagengtm7@gmail.com>
2023-12-01 17:21:45 -0500

    fixed the day one solution to be more efficient

--- 2023/1/dayone.py
+++ 2023/1/dayone.py
@@ -3,40 +3,20 @@
 
 file = open('input.txt','r')
 content = file.readlines()
-
-# crack pot way of allowing overlapping numbers
-# need to find better solution but for now works
-def update_string(string):
-    if "three" in string:
-        string = string.replace("hre", "3")
-    if "four" in string:
-        string = string.replace("our", "4")
-    if "five" in string:
-        string = string.replace("iv", "5")
-    if "seven" in string:
-        string = string.replace("eve", "7")
-    if "eight" in string:
-        string = string.replace("igh", "8")
-    if "nine" in string:
-        string = string.replace("in", "9")
-    if "one" in string:
-        string = string.replace("ne", "1")
-    if "two" in string:
-        string = string.replace("two", "2")
-    if "six" in string:
-        string = string.replace("six", "6")
-    return(string) 
-
 # placeholder for total per line
 total = []
+num_dict = {'1':'one', '2': 'two', '3':'three','4':'four', '5':'five', '6':'six', '7':'seven' , '8':'eight', '9':'nine'}
 #for each line, replace the spelled numbers
 for line in content:
     number = ''
-    line = update_string(line)
-    # for each char add to placeholder per line
-    for char in line:
+    #line = update_string(line)
+    # enumerate lets you use the index of each character in the line
+    for i, char in enumerate(line):
         if char.isdigit():
             number += char
+        for num in num_dict:
+            if line[i:].startswith(num_dict.get(num)):
+                number += num
     # reduce down to first and last numbers
     if len(number) > 2:
         number = number[0] + number[len(number)-1]
